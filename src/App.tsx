a324953074cd791ca88cd80e68fd489777a16eac
Phoenix Zhong <phoenixson191@gmail.com>
2025-06-08 10:53:09 +0800

    feat: bun + react + tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <div className='w-[80%] min-h-screen mx-auto px-4 py-4 flex flex-col justify-center items-center'>
+        <div className='w-full text-center'>
+          <h1 className='text-2xl'>Rsbuild with React</h1>
+        </div>
+        <div className='w-full text-center'>
+          <p>Start building amazing things with Rsbuild.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
